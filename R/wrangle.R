69cac725ca1deefe82cc77c8aae1fd86388f3297
Anders Gravbrøt Finstad <anders.finstad@ntnu.no>
2017-11-06 11:46:24 +0100

    updated wrangle.R

--- R/wrangle.R
+++ R/wrangle.R
@@ -17,7 +17,8 @@
 #' inndata <- readRDS("./testdata/view_occurrence_by_event.rds")
 #' geoselect_native <- readRDS("./testdata/geoselect_native_Esox_lucius.rds")
 #' focal_species <- "Esox lucius"
-#' wrangle_and_slice(inndata=inndata,start_year=1970,end_year=2017,)
+#' mynewdata_aslist <- wrangle_and_slice(inndata=inndata,start_year=1970,end_year=2017)
+#' mynewdata <- outdata[["data"]]
 #'
 #' @import dplyr
 #' @import tidyr
@@ -31,10 +32,10 @@
 #'
 #'
 
-f_wrangle_and_slice <- function(start_year,end_year,inndata,focal_species,geoselect_native){
+wrangle_and_slice <- function(start_year,end_year,inndata,focal_species,geoselect_native){
 
   #######################################################
-  # Reclassify establishMeans of focal species
+  # Reclassify establishMeans of focal species ----------------------------
   # based upon distr. polygon
   #######################################################
   inndata_species <- inndata[inndata$scientificName==focal_species,]
@@ -45,25 +46,30 @@ f_wrangle_and_slice <- function(start_year,end_year,inndata,focal_species,geosel
 
   # convert inndata to SpatialPointDataFrame
   #inndata<-outdata_timeslot
-  inndata <- as.data.frame(inndata)
-  coordinates(inndata) = ~decimalLongitude + decimalLatitude
+  inndata_species <- as.data.frame(inndata_species)
+  coordinates(inndata_species) = ~decimalLongitude + decimalLatitude
 
   # project outdata_event
   latlong = "+init=epsg:4326"
-  proj4string(inndata) = CRS(latlong)
+  proj4string(inndata_species) = CRS(latlong)
 
   # filter out values from outdata_event based upon geoselect_spdf
   # Note: clipping spatial data in R by using [] from http://robinlovelace.net/r/2014/07/29/clipping-with-r.html
-  inndata2 <- inndata[geoselect_spdf, ]
+  inndata_geoselect <- inndata_species[geoselect_spdf, ]
 
-  inndata$establishmentMeans <- ifelse(inndata$waterBodyID %in% inndata2@data$waterBodyID & inndata$scientificName==focal_species,"native",
+  inndata_species$establishmentMeans <- ifelse(inndata_species$waterBodyID %in% inndata_geoselect@data$waterBodyID,"native",
                                        "introduced")
-  inndata <- as.data.fame(inndata)
+  inndata_species <- as.data.frame(inndata_species)
+  inndata_species <- inndata_species %>% select(eventID,establishmentMeans_tmp=establishmentMeans)
+
+  inndata <- left_join(inndata,inndata_species,by="eventID")
+  inndata$establishmentMeans <- ifelse(inndata$scientificName==focal_species,
+                                       as.character(inndata$establishmentMeans_tmp),inndata$establishmentMeans)
 
   #######################################################
-  # wrangle data to wide format by species
+  # spread data to wide format by species -----------------------------
   #######################################################
-  inndata <- inndata@data %>% mutate(scientificName2=str_replace(scientificName, " ", "_"))
+  inndata <- inndata %>% mutate(scientificName2=str_replace(scientificName, " ", "_"))
   inndata$year <- year(parse_date_time(inndata$dateEnd,"ymd"))
   inndata <- inndata[complete.cases(inndata$year),]
 
@@ -71,154 +77,129 @@ f_wrangle_and_slice <- function(start_year,end_year,inndata,focal_species,geosel
   spread_data$presence <- 1
   spread_data <- spread_data %>% spread(scientificName2,presence,fill=0)
 
-  # select variables to be included. NB! No occurrence level variables, only event level and above
-  inndata2 <- inndata %>% dplyr::select(dateEnd,year,
-                                        recordedBy,samplingTaxaRange,samplingProtocol,
-                                        datasetID,county,municipality,countryCode,eventID,#decimalLatitude,decimalLongitude,
-                                        utm_x,utm_y,waterBodyID,area_km2,distance_to_road,
-                                        perimeter_m,elevation,eurolst_bio01,eurolst_bio02,
-                                        eurolst_bio03,eurolst_bio05,eurolst_bio06,eurolst_bio07,
-                                        eurolst_bio10,eurolst_bio11) %>%
-    distinct()
-  inndata_event <- left_join(inndata2,spread_data,by="eventID")
+  # # select variables to be included. NB! No occurrence level variables, only event level and above
+  # inndata2 <- inndata %>% dplyr::select(dateEnd,year,eventID,scientificName,
+  #                                       recordedBy,samplingTaxaRange,samplingProtocol,
+  #                                       datasetID,county,municipality,countryCode,eventID,#decimalLatitude,decimalLongitude,
+  #                                       utm_x,utm_y,waterBodyID,area_km2,distance_to_road,
+  #                                       perimeter_m,elevation,eurolst_bio01,eurolst_bio02,
+  #                                       eurolst_bio03,eurolst_bio05,eurolst_bio06,eurolst_bio07,
+  #                                       eurolst_bio10,eurolst_bio11) %>%
+  #   distinct()
+  inndata2 <- left_join(inndata,spread_data,by="eventID")
 
-  #######################################
-  # Make shore line complexity variable #
-  #######################################
-  #make SCI, and index portraying shore line complexity (see Voje etal. 2013 for similar approach)
 
-  inndata_event$SCI<-(inndata_event$perimeter_m/1000)/(2*sqrt(pi*inndata_event$area_km2))
 
   #######################################################
-  # Slice by timeslot and stack output
+  # Get distance to closest pop at start of time slot----
   #######################################################
 
-  # time-periods (timeslot)for which to loop over defined by "timeslot".
-  # start year defined in input variable "startyear"
-  # species defined in input variable "species
+  # Create data.frame with all introductions of focal-species
+  # within the given time-slot given as introduction=1,
+  # distance_to_closest population of focal species
+  # given as distance at start of time-slot
 
-  # create two vectors of max and min year of timeslot
-  n_timeslot <-  round((max(inndata$year) - startyear)/timeslot,0) # define number of timeslots
-  t_slot_start <- max(inndata$year)-(timeslot*seq(1:n_timeslot))
-  t_slot_end <- max(inndata$year)-(timeslot*seq(1:n_timeslot))+timeslot
+  # Select locations of all populations of focal species at start of timeslot i.
+  # Useatd for calcualation distance to closest population
+  data1 <- inndata2 %>%
+    filter(year>=start_year,scientificName==focal_species) %>%
+    dplyr::select(utm_x,utm_y,waterBodyID) %>% distinct()
+
+  # Select all locations for which there where observations of fish
+  data2 <- inndata2 %>%
+    filter(year<=end_year) %>%
+    dplyr::select(utm_x,utm_y,waterBodyID) %>% distinct()
 
 
-  # start loooooping.....
-  for (j in 1:length(species)){
-    for (i in 1:length(t_slot_start)){
+  # Calculate distance to closest population of focal species -------
+  # at start of time-slot for all locations with fish observations at end
+  # of time-slot i. Use get.knnx from the FNN package.
+  nn <- get.knnx(data1[c("utm_x","utm_y")],data2[c("utm_x","utm_y")],2)
 
-      # Select locations of all populations of focal species at start of timeslot i.
-      # Used for calcualation distance to closest population
-      data1 <- inndata %>%
-        filter(year<=t_slot_start[i],scientificName==species[j]) %>%
-        dplyr::select(utm_x,utm_y,waterBodyID) %>% distinct()
+  dist_to_closest_pop <- ifelse(nn$nn.dist[,1]==0,nn$nn.dist[,2],nn$nn.dist[,1])
+  waterBodyID <- data2$waterBodyID
+  distance_data <- as.data.frame(cbind(dist_to_closest_pop,waterBodyID))
+  # run average distance for each waterBodyID (the problem of duplicated
+  # waterBodyIDs here comes from input coords may resemble both outlet
+  # or centroid of waterbody)
+  distance_data <- distance_data %>%
+    group_by(waterBodyID) %>%
+    summarize(dist_to_closest_pop=mean(dist_to_closest_pop))
 
-      # Select all locations for which there where observations of
-      # fish at the end of time-slot i.
-      data2 <- inndata %>%
-        filter(year<=t_slot_end[i]) %>%
-        dplyr::select(utm_x,utm_y,waterBodyID) %>% distinct()
+  # match back with inndata
+  inndata3 <- left_join(inndata2,distance_data,by="waterBodyID")
 
-      # Calculate distance to closest population of focal species -------
-      # at start of time-slot for all locations with fish observations at end
-      # of time-slot i. Use get.knnx from the FNN package.
-      nn <- get.knnx(data1[c("utm_x","utm_y")],data2[c("utm_x","utm_y")],2)
 
-      dist_to_closest_pop <- ifelse(nn$nn.dist[,1]==0,nn$nn.dist[,2],nn$nn.dist[,1])
-      waterBodyID <- data2$waterBodyID
-      distance_data <- as.data.frame(cbind(dist_to_closest_pop,waterBodyID))
+  # Add 0/1 column of introductions occuring during timeslot --------.
+  # In order for this to classify as an introduction event,
+  # we first need to check out that the species not have been
+  # observed in the given waterbody before.
 
-      # create occurrence data of fish j in time-slot i ----
-      # i.e. populations for which there are observation of fish at end of timeslot i).
-      # Merge inn distance to closest population of focal species
-      outdata_temp <- inndata_event %>% filter(year<=t_slot_end[i])
-      outdata_temp <- left_join(outdata_temp,distance_data,by="waterBodyID")
-      outdata_temp$focal_species <- species[j]
-      outdata_temp$t_slot_end <- t_slot_end[i]
-      outdata_temp$t_slot_start <- t_slot_start[i]
-      outdata_temp$t_slot <- i
+  wb_fish_j <- inndata3 %>% filter(scientificName==focal_species,year<=start_year) %>%
+    dplyr::select(waterBodyID)
 
-      # Add 0/1 column of introductions occuring during timeslot --------.
-      # In order for this toclassify as an introduction event,
-      # we first need to check out that the species not have been
-      # observed in the given waterbody before.
+  # Then, select events with observation of focal species in time-slot,
+  # where focal species are classified as introduced.
+  outdata_temp2 <- inndata3 %>%
+    dplyr::filter(scientificName==focal_species,
+                                             year<=end_year,
+                                             year>=start_year,
+                                             establishmentMeans=="introduced",
+                                             !waterBodyID %in% wb_fish_j$waterBodyID) %>%
+    dplyr::select(eventID)
 
-      wb_fish_j <- inndata %>% filter(scientificName==species[j],year<=t_slot_start[i]) %>%
-        dplyr::select(waterBodyID)
+  # Then classify events as introduction events of species j.
+  inndata3$introduced <- ifelse(inndata$eventID %in% outdata_temp2$eventID,
+                                    1,0)
 
-      # Then, select events with observation of focal species in time-slot,
-      # where focal species are classified as introduced.
-      outdata_temp2 <- inndata %>% dplyr::filter(scientificName==species[j],
-                                                 year<=t_slot_end[i],
-                                                 year>=t_slot_start[i],
-                                                 establishmentMeans=="introduced",
-                                                 !waterBodyID %in% wb_fish_j$waterBodyID) %>%
-        dplyr::select(eventID)
-      # Then classify events as introduction events of species j.
-      outdata_temp$introduced <- ifelse(outdata_temp$eventID %in% outdata_temp2$eventID,
-                                        1,0)
-
-      # append above time-slot data to outdata,
-      # if outdata does not exist (i.e. j=1,i=1), create outdata on the fly
-      if (!exists("outdata_event")){
-        outdata_event <- outdata_temp
-      } else {
-        outdata_event <- bind_rows(outdata_event,outdata_temp)
-      }
-
-    } # end of i loop
-  } # end of j loop
+  inndata3$focal_species <- focal_species
 
 
   #########################################################################################
-  # Group by time-slot, waterBodyID and species -----
-  # output is time-slot averaged data (aggregating individual events)
+  # Group by waterBodyID and species -----
   #######################################################################################
 
-  outdata_timeslot <- outdata_event %>%
-    group_by(focal_species,t_slot,t_slot_start,t_slot_end,waterBodyID,
-             county,municipality,countryCode) %>%
-    summarise(introduced=max(introduced),
-              dist_to_closest_pop=mean(dist_to_closest_pop))
+  inndata4 <- inndata3 %>%
+    group_by(waterBodyID,county,municipality,countryCode) %>%
+    summarise(dist_to_closest_pop=mean(dist_to_closest_pop))
 
   # get presence/absence aggregated for each time-slot,waterBody and focal species
   # and join to outdata_timeslot
 
-  # get name vector of species presence/absence variables and selectd on outdata_event
-  specieslist <-as.character(names(spread_data)[2:length(names(spread_data))])
-  specieslist2 <- specieslist[specieslist %in% names(outdata_event)]
+  # select out the numeric variables from innndata to aggregate on by waterbody
+  tmp <- inndata3 %>% select_if(is.numeric)
 
-  tmp <- outdata_event[, c("focal_species","t_slot","SCI","area_km2",
-                           "distance_to_road","elevation","waterBodyID",
-                           "decimalLatitude","decimalLongitude","utm_x","utm_y",
-                           "eurolst_bio01","eurolst_bio10","eurolst_bio11",
-                           specieslist2)]
-  aggdata2<- tmp %>% group_by(focal_species,t_slot,waterBodyID) %>% summarise_all(funs(max))
-  outdata_timeslot <- left_join(outdata_timeslot,aggdata2,
-                                by = c("focal_species", "t_slot", "waterBodyID"))
+  aggdata2 <- tmp %>% group_by(waterBodyID) %>% summarise_all(funs(max))
+  aggdata2$focal_species <- focal_species
 
   # remove all presence/absence data for species not recorded in selected area
   # (to clean up outdata)
+  specieslist2 <- unique(inndata3$scientificName2)
+  colsToRemove <- specieslist2[colSums(aggdata2[specieslist2])==0]
+  aggdata2 <- aggdata2[!names(aggdata2) %in% colsToRemove]
+  aggdata2 <- aggdata2 %>% select(-dist_to_closest_pop)
 
-  colsToRemove <- specieslist2[colSums(outdata_timeslot[specieslist2])==0]
-  outdata_timeslot <- outdata_timeslot[!names(outdata_timeslot) %in% colsToRemove]
+  # link back to waterbody etc...
+  aggdata3 <- left_join(inndata4,aggdata2,by="waterBodyID")
 
-  # transform some variables from model
-  outdata_timeslot$area_km2_log<-log(outdata_timeslot$area_km2)
-  outdata_timeslot$t_slot<-as.factor(outdata_timeslot$t_slot)
-  outdata_timeslot$introduced<-as.integer(outdata_timeslot$introduced)
-  outdata_timeslot$dist_to_closest_pop_log<-log(outdata_timeslot$dist_to_closest_pop)
-  outdata_timeslot$county<-as.factor(outdata_timeslot$county)
-  outdata_timeslot$distance_to_road<-outdata_timeslot$distance_to_road+0.1
-  outdata_timeslot$distance_to_road_log<-log(outdata_timeslot$distance_to_road)
+  #######################################
+  # Add some derived variables on the fly -------------------------
+  #######################################
+  #make SCI, and index portraying shore line complexity (see Voje etal. 2013 for similar approach)
+
+  aggdata3$SCI<-(aggdata3$perimeter_m/1000)/(2*sqrt(pi*aggdata3$area_km2))
+  aggdata3$area_km2_log<-log(aggdata3$area_km2)
+  aggdata3$introduced<-as.integer(aggdata3$introduced)
+  aggdata3$dist_to_closest_pop_log<-log(aggdata3$dist_to_closest_pop)
+  aggdata3$county<-as.factor(aggdata3$county)
+  aggdata3$distance_to_road<-aggdata3$distance_to_road+0.1
+  aggdata3$distance_to_road_log<-log(aggdata3$distance_to_road)
 
   outdata <- list()
-  outdata[[1]] <- outdata_event
-  outdata[[2]] <- outdata_timeslot
-
-  # write outdata to file as RDS object
-  saveRDS(outdata_timeslot,"./Data/outdata_timeslot.rds")
-  saveRDS(outdata_event,"./Data/outdata_event.rds")
+  outdata[["data"]] <- aggdata3
+  outdata[["start_year"]] <- start_year
+  outdata[["end_year"]] <- end_year
+  outdata[["focal_species"]] <- focal_species
 
   return(outdata)
 
