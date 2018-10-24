2adcded2a8555d62cf471e3a113a220bc445924a
Stefan Blumentrath <stefan@vm-srv-wallace.vm.ntnu.no>
2018-10-24 12:44:27 +0200

    make DB connection independent from RStudio API

--- R/get_lake_environment.R
+++ R/get_lake_environment.R
@@ -5,9 +5,15 @@ library(pool)
 pg_drv <- RPostgreSQL::PostgreSQL()
 pg_host <- "vm-srv-wallace.vm.ntnu.no"
 pg_db <- 'nofa'
-pg_user <- rstudioapi::askForPassword("enter username")
-pg_password <- rstudioapi::askForPassword("enter psw")
-
+user_msg <- 'Please enter your user:'
+pw_msg <- "Please enter your password:"
+if (any(grepl("RStudio", .libPaths()))) {
+  pg_user <- rstudioapi::askForPassword(user_msg)
+  pg_password <- rstudioapi::askForPassword(pw_msg)
+} else {
+  pg_user <- readline(prompt=user_msg)
+  pg_password <- readline(prompt=pw_msg)
+}
 
 pool <- dbPool(
   drv = pg_drv,
@@ -21,13 +27,13 @@ pool <- dbPool(
 con <- poolCheckout(pool)
 
 ####################################################################################
-# Nå
+# N?
 
-### Hent ut miljødata for alle innsjøer i Agder
+### Hent ut milj?data for alle innsj?er i Agder
 get_lake_environment <- function(db_conection, waterBodyIDs) {
   sql_string <- paste("SELECT * FROM nofa.view_lake_environment AS a WHERE \"waterBodyID\" IN (", toString(waterBodyIDs, sep=','), ");", sep='')
   res <- dbGetQuery(db_conection, sql_string)
   res
 }
 # Eksempel
-lake_env <- get_lake_environment(con, wbids)
+# lake_env <- get_lake_environment(con, wbid_wrid$waterBodyID)
