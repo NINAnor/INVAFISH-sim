3541ec423a926246004b97543bea460b92816f51
Stefan Blumentrath <stefan@vm-srv-wallace.vm.ntnu.no>
2018-11-04 20:56:19 +0100

    use only central DB connection

--- R/get_location_environment.R
+++ R/get_location_environment.R
@@ -1,29 +1,28 @@
-library(RPostgreSQL)
-library(pool)
+# library(RPostgreSQL)
+# library(pool)
 
-#Set connection parameters
-pg_drv <- RPostgreSQL::PostgreSQL()
-pg_host <- "vm-srv-finstad.vm.ntnu.no"
-pg_db <- 'nofa'
-pg_user <- rstudioapi::askForPassword("enter username")
-pg_password <- rstudioapi::askForPassword("enter psw")
+# # Set connection parameters
+# pg_drv <- RPostgreSQL::PostgreSQL()
+# pg_host <- "vm-srv-finstad.vm.ntnu.no"
+# pg_db <- 'nofa'
+# pg_user <- rstudioapi::askForPassword("enter username")
+# pg_password <- rstudioapi::askForPassword("enter psw")
 
+# pool <- dbPool(
+#   drv = pg_drv,
+#   dbname = pg_db,
+#   host = pg_host,
+#   user = pg_user,
+#   password = pg_password,
+#   idleTimeout = 36000000
+# )
 
-pool <- dbPool(
-  drv = pg_drv,
-  dbname = pg_db,
-  host = pg_host,
-  user = pg_user,
-  password = pg_password,
-  idleTimeout = 36000000
-)
-
-con <- poolCheckout(pool)
+# con <- poolCheckout(pool)
 
 ####################################################################################
-# Nå
+# N?
 
-### Hent ut miljødata for alle innsjøer i Agder
+### Hent ut milj?data for alle innsj?er i Agder
 get_location_environment <- function(db_conection) {
   sql_string <- "SELECT * FROM nofa.view_location_environment AS a WHERE county IN ('Vest-Agder', 'Aust-Agder');"
   res <- dbGetQuery(db_conection, sql_string)
