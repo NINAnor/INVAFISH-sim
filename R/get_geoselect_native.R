a7e8fb1be9353210ccea55e2ba658e33af219193
Kim Bærum <kim.barum@ninrstudio04.nina.no>
2020-10-08 12:21:47 +0200

    Added script

--- R/get_geoselect_native.R
+++ R/get_geoselect_native.R
@@ -21,17 +21,6 @@ get_historic_distribution <- function(connection, taxonID) {
   species_native <- get_postgis_query(con, paste0('SELECT
                                                  ST_Transform(geom, 4326) AS geom, occurrence
                                                  FROM nofa."l_taxon_historicDistribution"
-                                                 WHERE "taxonID" = ', taxonID),  geom_name = "geom")
+                                                 WHERE occurrence > 1 AND "taxonID" = ', taxonID),  geom_name = "geom")
   species_native
 }
-
-
-geoselect_no_gjedde_pop_5000 <- dbGetQuery(con, 'SELECT al.id AS "waterBodyID", count(ol.geom) FROM
-                                                  nofa.lake AS al,
-                                           (SELECT geom FROM nofa.lake WHERE id IN (SELECT "waterBodyID" FROM nofa.get_last_occurrence_status(
-                                           "taxonID" => 26181,
-                                           counties => \'Vest-Agder,Aust-Agder\'))) AS ol
-                                           WHERE ST_DWithin(al.geom, ol.geom, 5000)
-                                           GROUP BY al.id')
-
-
